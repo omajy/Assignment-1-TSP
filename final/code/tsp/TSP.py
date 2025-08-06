92544ffdf83d71f7798a08cb431875b747c96385
Otto-gits <ottohedger@gmail.com>
2025-08-06 14:31:05 +0930

    Euclidian distance functionality added with an improved testing as well

--- final/code/tsp/TSP.py
+++ final/code/tsp/TSP.py
@@ -2,11 +2,15 @@ import math
 import os
 class TSP:
     # loads a TSP file and parses it into a data structure
-    def __init__(self):
+    def __init__(self, filename=None):
         #holds the number of cities, a dictionary of city coordinates, and a distance matrix
         self.num_cities = 0
         self.cities = {}
         self.dist_matrix = []
+        
+        if filename:
+            self.load_from_file(filename)
+            self.build_distance_matrix()
         
     def load_from_file(self, filename):
         # Reads the .tsp file from our TSPlib_data folder and begins to parse it into our data structure
@@ -28,8 +32,26 @@ class TSP:
                         #split each line into parts and inputs them into our cities dictionary
                         
                         parts = city_line.split()
-                        city_id, x, y = int(parts[0]), float(parts[1]), float(parts[2])
+                        city_id, x, y = int(parts[0]) - 1, float(parts[1]), float(parts[2])  # Adjusting index to be 0-based
                         self.cities[city_id] = (x, y)
                         
                     break
+
+    def build_distance_matrix(self):
+        # Builds a distance matrix from the cities dictionary
+        
+        #initialize the distance matrix with zeros
+        self.dist_matrix = [[0.0] * self.num_cities for _ in range(self.num_cities)]
+        
+        #Get the coordinates of each city and calculate the euclidian distance between the two cities
+        for i in range(self.num_cities):
+            xi, yi = self.cities[i]
+            for j in range(i+1, self.num_cities):
+                if i != j:
+                    #calculate the Euclidean distance between cities i and j
+                    xj, yj = self.cities[j]
+                    self.dist_matrix[i][j] = self.dist_matrix[j][i] = math.sqrt(((xi - xj) ** 2) + ((yi - yj) ** 2))
+
+
+
 
